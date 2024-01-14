bd2e8ec13754df253d758835dee40f6dabe98308
thatrtxdude <phillip.oertel.hamm@gmail.com>
2024-01-14 16:46:28 +0100

    some fixes

--- script.js
+++ script.js
@@ -59,33 +59,22 @@ document.addEventListener('DOMContentLoaded', function () {
         inputElement.focus();
     }
 
-    let audio = new Audio('sounds/1.mp3');
     function handleCommand(command) {
         switch (command.toLowerCase()) {
             case 'ls':
                 return 'info.txt setup.txt socials.txt credits.txt';
             case 'cat info.txt':
-                return "placeholder text";
+                return "professional slop creator, wannabe level designer, whatever you wanna call me. i make shit for the source engine, be it maps, models or textures. i do a bit of everything. besides animating, fuck that.";
             case 'cat setup.txt':
-                return 'XFX Radeon 6800XT, Ryzen 7 5800x3D, 32GB DDR4 3200Mhz, Razer Huntsman Mini, Razer Deathadder v2 X Hyperspeed, Beyerdynamic DT770 PRO, NZXT Capsule Mic, Logitech G29 + H-Shifter';
+                return 'XFX Radeon 6800XT, Ryzen 7 5800x3D, 32GB DDR4 3200Mhz, Razer Huntsman Mini, Razer Deathadder v2 X Hyperspeed, Beyerdynamic DT770 PRO, NZXT Capsule Mic, Logitech G27 + H-Shifter';
             case 'cat socials.txt':
                 return '<a href="https://steamcommunity.com/id/thatrtxdude/">Steam</a> | <a href="https://github.com/thatrtxdude">Github</a> | <a href="https://twitter.com/thatrtxdude_">Twitter/X</a> | Discord: thatrtxdude | Matrix: @thatrtxdude:matrix.org';
             case 'help':
                 return 'Available commands: ls (List all files), cat (Open .txt files), help, clear';
             case 'cat credits.txt':
-                return "everything - me (there a bunch of easter eggs by the way)";
-            case 'kissinger':
-                return 'Please piss on his grave';
-            case 'rorta':
-                return 'Insertion will be finished soon (trust)'
-            case 'subahibi':
-                audio.volume = 0.1;
-                audio.play();
-                return 'Live happily! (type mute to stop the music)'
-            case 'mute':
-                audio.pause();
-                audio.currentTime = 0;
-                return ''
+                return "me, thatrtxdude, for everything";
+            case 'gmod eft ready or not realism reshade 4k ultra hd hdr lighting':
+                return "oh may the lord smite you down";
             case 'cat':
                 return 'Please enter a file to open!'
             case 'clear':
